5e1313f0ae74cf2397a5d4afb70a751dff9e4e31
vpslinuxinstall <shoparoundwyz@gmail.com>
2019-12-05 12:21:04 -0500

    commit

--- src/test/smallest.java
+++ src/test/smallest.java
@@ -14,6 +14,7 @@ public class smallest {
 
         }
 
+        // Bubble sort
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
